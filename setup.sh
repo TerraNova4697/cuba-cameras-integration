49abb6bd92df04dbfbe5831c3bee919b1fbf6e95
TerraNova4697 <nikitabechthold@gmail.com>
2024-04-12 10:23:29 +0500

    daemon service

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y python3 python3-pip python3-dev
+sudo pip3 install -r requirements.txt
+sudo mv /cuba/cuba-cameras-integration/cuba-cameras-integration.service /lib/systemd/system/cuba-cameras-integration.service
+sudo systemctl daemon-reload
