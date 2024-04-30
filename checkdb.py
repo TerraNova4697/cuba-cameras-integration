92d67bec2244ecb7f7798d71e5ee6abf32ddc1aa
TerraNova4697 <nikitabechthold@gmail.com>
2024-04-30 11:32:11 +0500

    Fix DB missing items

---
+++ checkdb.py
@@ -0,0 +1,33 @@
+import csv
+from database import engine
+from sqlalchemy.orm import Session
+from database import get_camera_by_name, get_all_cameras, create_camera  # noqa
+import json
+
+
+cameras = get_all_cameras()
+print(len(cameras))
+
+
+with open("list.csv", "r") as cameras_list:
+    with Session(engine) as session:
+        reader = csv.reader(cameras_list)
+        lack_rows = []
+        for row in reader:
+            if row[2] != "UUID":
+                camera = get_camera_by_name(row[0])
+                if not camera:
+                    print(row[0], row[2], row[4])
+                    lack_rows.append(
+                        {"name": row[0], "id": row[2], "ip": row[5], "ping_period": 60}
+                    )
+        with open("data.json", "w") as f:
+            json.dump(lack_rows, f, ensure_ascii=False, indent=2)
+        print(len(lack_rows))
+
+
+with open("data.json") as f:
+    data = json.load(f)
+
+    for item in data:
+        create_camera(**item)
