ecc22182c38c7b63eaa3e38cfccb7ff5ef927d8f
Nik <nikitabechthold@gmail.com>
2025-04-30 11:05:24 +0500

    added env example file

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 python3 -m venv .venv &&
 sudo .venv/bin/pip install -r requirements.txt
+
+cp env_example .env
+
 sudo cp cuba-cameras-integration.service /etc/systemd/system/cuba-cameras-integration.service
 sudo systemctl daemon-reload
