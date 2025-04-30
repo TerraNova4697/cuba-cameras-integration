289550fd9258892d332fb4574c869a7b53098433
Nik <nikitabechthold@gmail.com>
2025-04-30 10:58:24 +0500

    Updated dependencies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # Install current project as daemon service.
 
-sudo apt update
-sudo apt install -y python3 python3-pip python3-dev
-sudo pip3 install -r requirements.txt
-sudo mv /cuba/cuba-cameras-integration/cuba-cameras-integration.service /lib/systemd/system/cuba-cameras-integration.service
+python3 -m venv .venv &&
+sudo .venv/bin/pip install -r requirements.txt
+sudo cp cuba-cameras-integration.service /etc/systemd/system/cuba-cameras-integration.service
 sudo systemctl daemon-reload
